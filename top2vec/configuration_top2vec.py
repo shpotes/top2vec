44f6dfb323dcb5377643167c780019594847eb19
Santiago Hincapie <shinca12@eafit.edu.co>
2022-02-17 22:27:42 -0500

    feat: :seedling: add model skeleton

---
+++ top2vec/configuration_top2vec.py
@@ -0,0 +1,15 @@
+from transformers.configuration_utils import PretrainedConfig
+from transformers.utils import logging 
+
+logger = logging.get_logger(__name__)
+
+class Top2VecConfig(PretrainedConfig):
+    r"""
+    :class:`Top2VecConfig` is the configuration class to store the configuration of a
+    :class:`~Top2VecModel`. It is used to instantiate a Top2Vec model according to the 
+    specified arguments, defining the defining the model architecture.
+
+    Configuration objects inherit from :class:`~transformers.PretrainedConfig` and can be
+    used to control the model outputs. Read the documentation from
+    :class:`~transformers.PretrainedConfig` for more information.
+    """
